092f10cbba45cfb4a3fb5424310b16a0de788a19
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-06-24 11:08:50 +0300

    fix

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+export default function healthReturn(char) {
+    if (char.health > 50) {
+      return 'healthy';
+    } if (char.health >= 15 && char.health <= 50) {
+      return 'wounded';
+    }
+    return 'critical';
+  }
